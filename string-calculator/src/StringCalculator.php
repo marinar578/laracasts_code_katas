ddfc0043460a1b1d7cebe79249783dca83a33b32
Marina Rozova <mrozova@imac-fk.local>
2017-02-22 10:36:26 -0500

    Make sixth test pass

--- string-calculator/src/StringCalculator.php
+++ string-calculator/src/StringCalculator.php
@@ -12,7 +12,7 @@ class StringCalculator
         foreach ($numbers as $number)
         {
             if ($number < 0) throw new InvalidArgumentException;
-            $solution += $number;
+            $solution += ($number < 1000) ? $number : 0;
         }
 
         return $solution;
