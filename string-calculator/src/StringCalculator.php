ac35c40799dc926bd7facac151ceed01b33cba69
Marina Rozova <mrozova@imac-fk.local>
2017-02-22 10:52:46 -0500

    Extract regex and exception code into their own methods

--- string-calculator/src/StringCalculator.php
+++ string-calculator/src/StringCalculator.php
@@ -7,17 +7,27 @@ class StringCalculator
 
     public function add($numbers)
     {
-        $numbers = preg_split('/\s*(,|\\\n)\s*/', $numbers);
+        $numbers = $this->parseNumbers($numbers);
         $solution = 0;
 
         foreach ($numbers as $number)
         {
-            if ($number < 0) throw new InvalidArgumentException('Invalid number provided: ' . $number);
+            $this->guardAgainstInvalidNumber($number);
             if ($number >= 1000) continue;
 
             $solution += $number;
         }
 
         return $solution;
+    }
+
+    private function guardAgainstInvalidNumber($number)
+    {
+        if ($number < 0) throw new InvalidArgumentException('Invalid number provided: ' . $number);
+    }
+
+    private function parseNumbers($numbers)
+    {
+        return preg_split('/\s*(,|\\\n)\s*/', $numbers);
     }
 }
