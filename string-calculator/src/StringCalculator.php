29a9051eb2bfe6455036871b7ce9287fe4d89ab1
Marina Rozova <mrozova@imac-fk.local>
2017-02-22 10:33:35 -0500

    Refactored StringCalculator code so that all previous tests pass and make fifth test pass

--- string-calculator/src/StringCalculator.php
+++ string-calculator/src/StringCalculator.php
@@ -7,6 +7,14 @@ class StringCalculator
     {
         $numbers = explode(',', $numbers);
 
-        return array_sum($numbers);
+        $solution = 0;
+
+        foreach ($numbers as $number)
+        {
+            if ($number < 0) throw new InvalidArgumentException;
+            $solution += $number;
+        }
+
+        return $solution;
     }
 }
