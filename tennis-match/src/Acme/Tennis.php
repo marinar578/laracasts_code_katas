791806c8f4c5a1c8f34f5f4716bd8ca5829809e7
Marina Rozova <mrozova@imac-fk.local>
2017-02-22 12:21:19 -0500

    Make fourth test pass

--- tennis-match/src/Acme/Tennis.php
+++ tennis-match/src/Acme/Tennis.php
@@ -17,6 +17,11 @@ class Tennis
 
     public function score()
     {
+        if ($this->player1->points == 3 &&$this->player2->points == 0)
+        {
+            return 'Forty-Love';
+        }
+
         if ($this->player1->points == 2 &&$this->player2->points == 0)
         {
             return 'Thirty-Love';
