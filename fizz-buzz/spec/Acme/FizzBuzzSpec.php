ccb67aa2ed6ab18cd021f4dd9b94927307bbe2ca
Marina Rozova <mrozova@imac-fk.local>
2017-02-22 15:57:59 -0500

    Add first test and make it pass

--- fizz-buzz/spec/Acme/FizzBuzzSpec.php
+++ fizz-buzz/spec/Acme/FizzBuzzSpec.php
@@ -7,8 +7,8 @@ use Prophecy\Argument;
 
 class FizzBuzzSpec extends ObjectBehavior
 {
-    function it_is_initializable()
+    function it_translates_1_for_fizzbuzz()
     {
-        $this->shouldHaveType('Acme\FizzBuzz');
+        $this->execute(1)->shouldReturn(1);
     }
 }
--- fizz-buzz/src/Acme/FizzBuzz.php
+++ fizz-buzz/src/Acme/FizzBuzz.php
@@ -4,4 +4,8 @@ namespace Acme;
 
 class FizzBuzz
 {
+    public function execute($number)
+    {
+        return 1;
+    }
 }
