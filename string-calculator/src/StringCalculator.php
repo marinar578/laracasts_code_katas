5cc2ee4351582ff376f90d098faa4a8888a4ece1
Marina Rozova <mrozova@imac-fk.local>
2017-02-22 10:48:07 -0500

    Make modified exception test pass

--- string-calculator/src/StringCalculator.php
+++ string-calculator/src/StringCalculator.php
@@ -1,5 +1,7 @@
 <?php
 
+use Prophecy\Exception\InvalidArgumentException;
+
 class StringCalculator
 {
 
@@ -10,7 +12,7 @@ class StringCalculator
 
         foreach ($numbers as $number)
         {
-            if ($number < 0) throw new InvalidArgumentException;
+            if ($number < 0) throw new InvalidArgumentException('Invalid number provided: ' . $number);
             if ($number >= 1000) continue;
 
             $solution += $number;
