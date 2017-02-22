954813532ce59e421eddbaac2b4c444e9e7552c3
Marina Rozova <mrozova@imac-fk.local>
2017-02-22 12:08:04 -0500

    Construct two new players in spec to use in tests

--- tennis-match/spec/Acme/TennisSpec.php
+++ tennis-match/spec/Acme/TennisSpec.php
@@ -2,11 +2,20 @@
 
 namespace spec\Acme;
 
+use Acme\Player;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 
 class TennisSpec extends ObjectBehavior
 {
+    function let()
+    {
+        $this->john = new Player('John Doe', 0);
+        $this->jane = new Player('Jane Doe', 0);
+
+        $this->beConstructedWith($this->john, $this->jane);
+    }
+
     function it_scores_a_scoreless_game()
     {
         $this->score()->shouldReturn('Love-All');
